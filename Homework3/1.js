4aa6e14c628339649ffc2c971edc21b280531450
Eugene_Pro <proskuryakov.e@list.ru>
2023-04-27 11:04:53 +0300

    first commit homework4

--- Homework3/1.js
+++ Homework3/1.js
@@ -1,12 +1,11 @@
 // Температура в С° -> температура в F
-function myScript() {
+function getTemperatureConversion(temp) {
+    const temperatureF = (9 / 5) * temp + 32;
+    return temperatureF;
+}        
+const clickButton = document.querySelector('button');
+clickButton.addEventListener('click', function() {
     const temperatureC = Number.parseFloat(prompt("Введите температуру в градусах С°"));
     alert(`\tтемпература в Цельсиях = ${temperatureC} C°,
-        температура в Фаренгейтах = ${+getTemperatureConversion(temperatureC).toFixed(1)} F`);
-    function getTemperatureConversion(temp) {
-        const temperatureF = (9 / 5) * temp + 32;
-        return temperatureF;
-    }
-}
-const clickButton = document.querySelector('button');        
-clickButton.addEventListener('click', myScript);
+        температура в Фаренгейтах = ${+getTemperatureConversion(temperatureC).toFixed(1)} F`);    
+});
